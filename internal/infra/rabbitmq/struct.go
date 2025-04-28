02aa03fab1d5a5def400214b12904fdf66720d9c
devanfer <ferrelvsc20@gmail.com>
2025-04-28 22:32:30 +0700

    chore: add rabbitmq connection retry

--- internal/infra/rabbitmq/struct.go
+++ internal/infra/rabbitmq/struct.go
@@ -2,6 +2,7 @@ package rabbitmq
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/devanfer02/ratemyubprof/internal/infra/env"
 	"github.com/rabbitmq/amqp091-go"
@@ -15,17 +16,37 @@ type RabbitMQ struct {
 }
 
 func NewRabbitMQ(env *env.Env, logger *zap.Logger) *RabbitMQ {
-	conn, err := amqp091.Dial(
-		fmt.Sprintf("amqp://%s:%s@%s:%d/",
-			env.RabbitMQ.User,
-			env.RabbitMQ.Password,
-			env.RabbitMQ.Host,
-			env.RabbitMQ.Port,
-		),
+	var (
+		retries = 5
+		conn *amqp091.Connection
+		err error 
 	)
-	if err != nil {
-		panic(err)
+	
+	for {
+		conn, err = amqp091.Dial(
+			fmt.Sprintf("amqp://%s:%s@%s:%d/",
+				env.RabbitMQ.User,
+				env.RabbitMQ.Password,
+				env.RabbitMQ.Host,
+				env.RabbitMQ.Port,
+			),
+		)
+		if err == nil {
+			break 
+		}
+
+		if (retries <= 0) {
+			logger.Error("Failed to connect to RabbitMQ after multiple attempts", zap.Error(err))
+			panic(err)
+		}
+
+		logger.Error("Failed to connect to RabbitMQ", zap.Error(err))
+		logger.Info("Retrying in 5 seconds...")
+
+		time.Sleep(5 * time.Second)
+		retries--
 	}
+
 
 	rabbitMQ := &RabbitMQ{
 		conn: conn,
